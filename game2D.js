cc11017590dc491d6a87d82bbf0783c02ff6d449
dekasaid <59932402+dekasaid@users.noreply.github.com>
2020-02-12 11:15:54 -0500

    modified the player function and html

--- game2D.js
+++ game2D.js
@@ -3,23 +3,71 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
+var deltaX = 0;
+var deltaY = 0;
 
-function Player()
-{
-    //drawing out the player 
+function drawTriangle() {
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    // the triangle
     ctx.beginPath();
-    ctx.moveTo(200,100);
-    ctx.lineTo(170,150);
-    ctx.lineTo(230, 150);
+    ctx.moveTo(200 + deltaX, 100 + deltaY);
+    ctx.lineTo(170 + deltaX, 150 + deltaY);
+    ctx.lineTo(230 + deltaX, 150 + deltaY);
     ctx.closePath();
+   
+    // the outline
     ctx.lineWidth = 10;
-    ctx.strokeStyle = "purple";
+    ctx.strokeStyle = "rgba(102, 102, 102, 1)";
     ctx.stroke();
-
-    ctx.fillStyle = "orange";
+   
+    // the fill color
+    ctx.fillStyle = "rgba(255, 204, 0, 1)";
     ctx.fill();
 
+    //window.requestAnimationFrame(drawTriangle);
+  }
+drawTriangle();
+
+ 
+window.addEventListener("keydown", keysPressed, false);
+window.addEventListener("keyup", keysReleased, false);
+ 
+var keys = [];
+ 
+function keysPressed(e) {
+    // store an entry for every key pressed
+    keys[e.keyCode] = true;
+ 
+    // left
+    if (keys[37]) {
+      deltaX -= 2;
+    }
+ 
+    // right
+    if (keys[39]) {
+      deltaX += 2;
+    }
+ 
+    // down
+    if (keys[38]) {
+      deltaY -= 2;
+    }
+ 
+    // up
+    if (keys[40]) {
+      deltaY += 2;
+    }
+ 
+    e.preventDefault();
+ 
+    drawTriangle();
 }
+ 
+function keysReleased(e) {
+    // mark keys that were released
+    keys[e.keyCode] = false;
+} 
 
 
 function Enemy(x, y)
@@ -47,20 +95,14 @@ Enemy.prototype.update = function(){
     }
 };
 
-
-
-
-
 var block = new Enemy(20, 25);
 var block1 = new Enemy(90, 40);
 var block2 = new Enemy(280, 220);
 
-function canvasUpdate()
-{
 
-ctx.fillStyle = "white";
-ctx.fillRect(0, 0, canvas.width, canvas.height);
+function drawCanvas()
 
+{ 
 
 block.update();
 block.draw();
@@ -71,8 +113,11 @@ block1.draw();
 block2.update()
 block2.draw();
 
-Player();
-window.requestAnimationFrame(canvasUpdate);
+window.requestAnimationFrame(drawCanvas);
+
 }
 
-canvasUpdate();
+drawCanvas();
+
+
+
