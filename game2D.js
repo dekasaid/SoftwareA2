e20986e90de07ff597230a069f7b9194eca0b628
Deka <59932402+dekasaid@users.noreply.github.com>
2020-02-11 21:11:23 -0500

    added player and enimies attributes

--- game2D.js
+++ game2D.js
@@ -1,46 +1,78 @@
 
-    can = document.getElementById("myCanvas"),
-    ctx = can.getContext("2d"),
-    width = "1200",
-    height = "700",
 
-    player = {
-        x : width/2,
-        y : height - 5,
-        width : 5,
-        height : 5,
-        movement : 2,
-        speedx : 0,
-        speedy : 0,
-        jump : false
-
-    },
-    
-    inputKey = [];
-
-    can.width = width;
-    can.height = height;
-    setInterval(draw, 100);
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
 
 
-function draw()
+function Player()
 {
-    //drawing the player 
-    ctx.clearRect(0,0,width,height);
-    ctx.fillStyle ="blue";
-    ctx.fillRect(player.x, player.y, player.width, player.height);
+    //drawing out the player 
+    ctx.beginPath();
+    ctx.moveTo(200,100);
+    ctx.lineTo(170,150);
+    ctx.lineTo(230, 150);
+    ctx.closePath();
+    ctx.lineWidth = 10;
+    ctx.strokeStyle = "purple";
+    ctx.stroke();
+
+    ctx.fillStyle = "orange";
+    ctx.fill();
 
 }
 
-window.addEventListener("load", function()
+
+function Enemy(x, y)
 {
-    draw();
-});
-document.body.addEventListener("keydown", function(event){
-    inputKey[event.keyCode] = true;
-});
+    this.x = x;
+    this.y = y;
+    this.width = 20;
+    this.height = 20;
+    this.direction = +1;
+}
 
-document.body.addEventListener("keyup", function(event){
-    inputKey[event.keyCode] = false;
-});
+Enemy.prototype.draw = function()
+{
+    ctx.fillStyle = "black";
+    ctx.fillRect(this.x, this.y, this.width, this.height);
 
+};
+
+Enemy.prototype.update = function(){
+
+    this.x = this.x + this.direction;
+    if(this.x <=0 || this.x + this.width >= canvas.width)
+    {
+        this.direction *= -1;
+    }
+};
+
+
+
+
+
+var block = new Enemy(20, 25);
+var block1 = new Enemy(90, 40);
+var block2 = new Enemy(280, 220);
+
+function canvasUpdate()
+{
+
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+
+block.update();
+block.draw();
+
+block1.update();
+block1.draw();
+
+block2.update()
+block2.draw();
+
+Player();
+window.requestAnimationFrame(canvasUpdate);
+}
+
+canvasUpdate();
